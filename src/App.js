8ff4e713c18cccf2a7de424402f1f4b5b5992288
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-04-30 21:36:02 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Alert,
+  Button,
+  Breadcrumb,
+  BreadcrumbItem,
+  CardProps,
+  Card,
+  CardImg,
+  Form,
+  FormGroup,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Form className="mb-4">
+            <Row>
+              <Col md>
+                <FormGroup controlId="formEmail">
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control type="email" placeholder="Example@gmail.com" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email address, trust us!
+                  </Form.Text>
+                </FormGroup>
+              </Col>
+              <Col md>
+                <FormGroup controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Button variant="secondary" type="submit">
+              Login
+            </Button>
+          </Form>
+          <Card className="mb-3" style={{ color: "#000" }}>
+            <CardImg src="https://picsum.photos/200/50" />
+            <Card.Body>
+              <Card.Title>Card Example</Card.Title>
+              <Card.Text>
+                This is and example of react bootstrap cards
+              </Card.Text>
+              <Button variant="primary">Read More</Button>
+            </Card.Body>
+          </Card>
+          <Breadcrumb>
+            <BreadcrumbItem active>Test1</BreadcrumbItem>
+            <BreadcrumbItem>Test2</BreadcrumbItem>
+            <BreadcrumbItem>Test3</BreadcrumbItem>
+          </Breadcrumb>
+          <Alert variant="success">This is a button</Alert>
+          <Button>Click</Button>
+        </Container>
       </header>
     </div>
   );
